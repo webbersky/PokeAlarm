d3216947e58c7bece9e04adb527ec7cf7de1fa0e
jesshub <jessnice7811@mail.com>
2017-04-21 14:13:52 +0100

    Update WebhookStructs.py

--- PokeAlarm/WebhookStructs.py
+++ PokeAlarm/WebhookStructs.py
@@ -13,11 +13,11 @@ from Crypto.Cipher import Blowfish #pip3 install pycrypto
 from base64 import b64encode, b64decode
 def blowfishEncrypt(Key, GPS):
     passw = bytes(Key, "UTF-8")
-	GPS = GPS.ljust(24)
-	GPS = bytes(GPS, "UTF-8")
-	cipher=Blowfish.new(passw, Blowfish.MODE_ECB)
-	encStr=b64encode(cipher.encrypt(GPS))
-	return encStr
+    GPS = GPS.ljust(24)
+    GPS = bytes(GPS, "UTF-8")
+    cipher=Blowfish.new(passw, Blowfish.MODE_ECB)
+    encStr=b64encode(cipher.encrypt(GPS))
+    return encStr
 
 ################################################## Webhook Standards  ##################################################
 
